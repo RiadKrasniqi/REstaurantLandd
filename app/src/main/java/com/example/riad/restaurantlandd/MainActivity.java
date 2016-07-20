0edcfedf988c549f99ba8df01261ca5a535d46eb
Riad <krasniqirijad@gmail.com>
2016-07-21 00:31:39 +0200

    First Commit

--- app/src/main/java/com/example/riad/restaurantlandd/MainActivity.java
+++ app/src/main/java/com/example/riad/restaurantlandd/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.riad.restaurantlandd;
-
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class MainActivity extends Activity {
+
+    ListView lv;
+    Context context;
+
+    ArrayList prgmName;
+    public static int [] prgmImages={R.drawable.restaurantapp,R.drawable.back};
+    public static String [] prgmNameList={"Let Us C","c++"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context=this;
+
+        lv=(ListView) findViewById(R.id.listView);
+        lv.setAdapter(new CustomAdapter(this, prgmNameList,prgmImages));
+
     }
+
+
+
 }
