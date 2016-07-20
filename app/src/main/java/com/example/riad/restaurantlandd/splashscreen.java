972d93ddb3fdbabec230e78c0adcb749de5c5d7f
Riad <krasniqirijad@gmail.com>
2016-07-20 21:45:19 +0200

    First Commit

---
+++ app/src/main/java/com/example/riad/restaurantlandd/splashscreen.java
@@ -0,0 +1,42 @@
+package com.example.riad.restaurantlandd;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+/**
+ * Created by Riad on 7/20/2016.
+ */
+public class splashscreen extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        final ImageView iv = (ImageView) findViewById(R.id.splashscreen);
+        final Animation an = AnimationUtils.loadAnimation(getBaseContext(),R.anim.scale);
+
+        iv.startAnimation(an);
+        an.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                finish();
+                Intent i = new Intent(getBaseContext(),MainActivity.class);
+                startActivity(i);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
