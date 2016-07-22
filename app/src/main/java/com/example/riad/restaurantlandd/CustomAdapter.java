12cd6945517fbe375352d4b04cf2f48a95b52791
Riad <krasniqirijad@gmail.com>
2016-07-22 16:37:18 +0200

    cv

--- app/src/main/java/com/example/riad/restaurantlandd/CustomAdapter.java
+++ app/src/main/java/com/example/riad/restaurantlandd/CustomAdapter.java
@@ -13,13 +13,19 @@ public class CustomAdapter extends BaseAdapter {
 
 
     String[] result;
+    String[] Lloji;
+    String[] vendndodhja;
+    String[] prishtine;
     Context context;
     int[] imageId;
     private static LayoutInflater inflater = null;
 
-    public CustomAdapter(MainActivity mainActivity, String[] prgmNameList, int[] prgmImages) {
+    public CustomAdapter(MainActivity mainActivity, String[] prgmNameList,String[] llojiii,String[] vendi,String[] country, int[] prgmImages) {
         // TODO Auto-generated constructor stub
         result = prgmNameList;
+        Lloji = llojiii;
+        vendndodhja = vendi;
+        prishtine = country;
         context = mainActivity;
         imageId = prgmImages;
         inflater = (LayoutInflater) context.
@@ -46,6 +52,9 @@ public class CustomAdapter extends BaseAdapter {
 
     public class Holder {
         TextView tv;
+        TextView tv2;
+        TextView tv3;
+        TextView tv4;
         ImageView img;
     }
 
@@ -56,8 +65,14 @@ public class CustomAdapter extends BaseAdapter {
         View rowView;
         rowView = inflater.inflate(R.layout.program_list, null);
         holder.tv = (TextView) rowView.findViewById(R.id.textView1);
+        holder.tv2= (TextView) rowView.findViewById(R.id.lloji);
+        holder.tv3 = (TextView) rowView.findViewById(R.id.vendi);
+        holder.tv4 = (TextView) rowView.findViewById(R.id.prishtine);
         holder.img = (ImageView) rowView.findViewById(R.id.imageView1);
         holder.tv.setText(result[position]);
+        holder.tv2.setText(Lloji[position]);
+        holder.tv3.setText(vendndodhja[position]);
+        holder.tv4.setText(prishtine[position]);
         holder.img.setImageResource(imageId[position]);
         rowView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,6 +83,13 @@ public class CustomAdapter extends BaseAdapter {
         });
         return rowView;
     }
+
+    @Override
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
+        notifyDataSetChanged();
+    }
+
 }
 
 
--- app/src/main/java/com/example/riad/restaurantlandd/MainActivity.java
+++ app/src/main/java/com/example/riad/restaurantlandd/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.riad.restaurantlandd;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
-import android.widget.SearchView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -13,13 +14,16 @@ public class MainActivity extends AppCompatActivity {
 
     ListView lv;
     Context context;
-    SearchView sv;
-    ArrayAdapter<String> adapter;
-    ArrayAdapter<Integer> adapteri;
 
     ArrayList prgmName;
-    public static int [] prgmImages={R.drawable.restaurantapp,R.drawable.back,R.drawable.back,R.drawable.back,R.drawable.back,R.drawable.back,R.drawable.back,R.drawable.back};
-    public static String [] prgmNameList={"Let Us C","c++","kulla","lopa","kali","peja","rrota","suxhuki"};
+    public static int [] prgmImages={R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher,R.mipmap.ic_launcher};
+    public static String [] prgmNameList={"Restaurant Kulla","c++","kulla","lopa","kali","peja","rrota","suxhuki","pela","makina"};
+    public static String [] country ={"Let Us C","c++","kulla","lopa","kali","peja","rrota","suxhuki","pela","makina"};
+    public static String [] llojiii ={"Let Us C:","c++:","kulla:","lopa:","kali:","peja:","rrota:","suxhuki:","pela:","makina:"};
+    public static String [] vendi ={"Let Us C","c++","kulla","lopa","kali","peja","rrota","suxhuki","pela","makina"};
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,43 +31,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-        context=this;
-
-        lv=(ListView) findViewById(R.id.listView);
-        sv = (SearchView) findViewById(R.id.searchView);
-        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,prgmNameList);
-
-
-
-
+        CustomAdapter adapter = new
+                CustomAdapter(MainActivity.this, prgmNameList,country,llojiii,vendi, prgmImages);
+        lv=(ListView)findViewById(R.id.listView);
         lv.setAdapter(adapter);
-
-
-
-
-        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String text) {
-                return false;
-            }
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
             @Override
-            public boolean onQueryTextChange(String text) {
-                adapter.getFilter().filter(text);
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(MainActivity.this, "You Clicked at " +prgmNameList[+ position], Toast.LENGTH_SHORT).show();
 
-                return false;
             }
         });
 
-
-
-
-
-
     }
-
-
-
-
 }
+
