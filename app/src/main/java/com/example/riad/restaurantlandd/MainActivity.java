6742d133312ccc74687a47f493fe182a7ab68cd6
Riad <krasniqirijad@gmail.com>
2016-07-22 13:57:09 +0200

    cv

--- app/src/main/java/com/example/riad/restaurantlandd/MainActivity.java
+++ app/src/main/java/com/example/riad/restaurantlandd/MainActivity.java
@@ -1,30 +1,68 @@
 package com.example.riad.restaurantlandd;
-import android.app.Activity;
+
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SearchView;
 
 import java.util.ArrayList;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     ListView lv;
     Context context;
+    SearchView sv;
+    ArrayAdapter<String> adapter;
+    ArrayAdapter<Integer> adapteri;
 
     ArrayList prgmName;
-    public static int [] prgmImages={R.drawable.restaurantapp,R.drawable.back};
-    public static String [] prgmNameList={"Let Us C","c++"};
+    public static int [] prgmImages={R.drawable.restaurantapp,R.drawable.back,R.drawable.back,R.drawable.back,R.drawable.back,R.drawable.back,R.drawable.back,R.drawable.back};
+    public static String [] prgmNameList={"Let Us C","c++","kulla","lopa","kali","peja","rrota","suxhuki"};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
         context=this;
 
         lv=(ListView) findViewById(R.id.listView);
-        lv.setAdapter(new CustomAdapter(this, prgmNameList,prgmImages));
+        sv = (SearchView) findViewById(R.id.searchView);
+        adapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,prgmNameList);
+
+
+
+
+        lv.setAdapter(adapter);
+
+
+
+
+        sv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String text) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String text) {
+                adapter.getFilter().filter(text);
+
+                return false;
+            }
+        });
+
+
+
+
+
 
     }
+
 
 
 
